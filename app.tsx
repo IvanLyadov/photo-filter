e5975ede76e3ececd03a8692ef5816c00483dd9b
IvanLyadov <dokaperi7@gmail.com>
2022-12-29 21:29:39 +0100

    Adding eslint, prettier to project

--- app.tsx
+++ app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 function App() {
     return (
-        <div>{'react setup from scratch without cra testing ts'}</div>
+        <><div>{'react setup from scratch without cra testing ts'}</div><div>test</div></>
     );
 }
 export default App;
